a37e40716ff741b20ab078d06f7ef51204ee2443
heatherciallella <heather.ciallella@rutgers.edu>
2018-10-27 19:21:12 -0400

    Completed code for part 2

--- set2.py
+++ set2.py
@@ -50,62 +50,72 @@ def uhf(p, rng):
 
 from bitarray import bitarray
 
-size = 2 ** 18  # size of the filter
-
-hash_fns = [uhf(find_prime(1000000000), size) for _ in range(5)]  # place holder for hash functions
-bloom_filter = bitarray(size)
-bloom_filter.setall(False)
-num_words = 0  # number in data stream
-num_words_in_set = 0  # number in Bloom filter's set
-num_positives = 0
-false_positives = 0
-
-for word in bloom_filter_set():  # add the word to the filter by hashing etc.
-    for location in [fn(int(''.join(str(ord(letter)) for letter in word))) for fn in hash_fns]:
-        if not bloom_filter[location]:
-            bloom_filter[location] = True
-
-    num_words_in_set += 1
-
-true_set = [word for word in bloom_filter_set()]
-
-for word in data_stream():  # check for membership in the Bloom filter
-    if all(bloom_filter[location] for location in
-           [fn(int(''.join(str(ord(letter)) for letter in word))) for fn in hash_fns]):
-        num_positives += 1
-
-        if word not in true_set:
-            false_positives += 1
-
-    num_words += 1
-
-fp_prob = (1 - e ** (-5 * num_words_in_set / size)) ** 5
-
-print('Total number of words in stream = %s' % (num_words,))
-print('Total number of words added to bloom filter = %s' % (num_words_in_set,))
-print('Total number of words in stream identified to be in bloom filter = %s' % (num_positives,))
-print('False positive probability = %s' % (fp_prob,))
-print('Expected number of false positives in stream = %s' % (fp_prob * num_words))
-print('Actual number of false positives in stream = %s' % (false_positives,))
+# size = 2 ** 18  # size of the filter
+#
+# hash_fns = [uhf(find_prime(1000000000), size) for _ in range(5)]  # place holder for hash functions
+# bloom_filter = bitarray(size)
+# bloom_filter.setall(False)
+# num_words = 0  # number in data stream
+# num_words_in_set = 0  # number in Bloom filter's set
+# num_positives = 0
+# false_positives = 0
+#
+# for word in bloom_filter_set():  # add the word to the filter by hashing etc.
+#     for location in [fn(int(''.join(str(ord(letter)) for letter in word))) for fn in hash_fns]:
+#         if not bloom_filter[location]:
+#             bloom_filter[location] = True
+#
+#     num_words_in_set += 1
+#
+# true_set = [word for word in bloom_filter_set()]
+#
+# for word in data_stream():  # check for membership in the Bloom filter
+#     if all(bloom_filter[location] for location in
+#            [fn(int(''.join(str(ord(letter)) for letter in word))) for fn in hash_fns]):
+#         num_positives += 1
+#
+#         if word not in true_set:
+#             false_positives += 1
+#
+#     num_words += 1
+#
+# fp_prob = (1 - e ** (-5 * num_words_in_set / size)) ** 5
+#
+# print('Total number of words in stream = %s' % (num_words,))
+# print('Total number of words added to bloom filter = %s' % (num_words_in_set,))
+# print('Total number of words in stream identified to be in bloom filter = %s' % (num_positives,))
+# print('False positive probability = %s' % (fp_prob,))
+# print('Expected number of false positives in stream = %s' % (fp_prob * num_words))
+# print('Actual number of false positives in stream = %s' % (false_positives,))
 
 ################### Part 2 ######################
+from statistics import median
 
 hash_range = 24  # number of bits in the range of the hash functions
-fm_hash_functions = [None] * 35  # Create the appropriate hashes here
+fm_hash_functions = [uhf(find_prime(1000000000), 2**hash_range) for _ in range(35)] # Create the appropriate hashes here
 
 def num_trailing_bits(n):
     """Returns the number of trailing zeros in bin(n)
 
     n: integer
     """
-    pass
+    n = format(n, 'b')
 
-num_distinct = 0
+    return len(n) - len(n.rstrip('0'))
 
-# for word in data_stream(): # Implement the Flajolet-Martin algorithm
-#    pass
+trailing_zeros = [[] for _ in range(35)]
 
-print("Estimate of number of distinct elements = %s" % (num_distinct,))
+for word in data_stream(): # Implement the Flajolet-Martin algorithm
+    word_as_number = int(''.join(str(ord(letter)) for letter in word))
+    i = 0
+
+    for fn in fm_hash_functions:
+        trailing_zeros[i].append(num_trailing_bits(fn(word_as_number)))
+        i += 1
+
+estimates = [2 ** max(i) for i in trailing_zeros]
+print("Estimate of number of distinct elements = %s" % (median([sum(estimates[i - 5:i]) / len(estimates[i - 5:i])
+                                                                for i in range(5, 36, 5)]),))
 
 ################### Part 3 ######################
 
